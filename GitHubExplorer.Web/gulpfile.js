cad902b4bf46cb58e6a27832be64236dc48c0fb6
Spyros Desyllas <sdesyllas@hotmail.com>
2017-02-09 16:09:19 +0000

    min css files and fonts with gulp-flatten

--- GitHubExplorer.Web/gulpfile.js
+++ GitHubExplorer.Web/gulpfile.js
@@ -1,3 +1,4 @@
+/// <binding BeforeBuild='clean' AfterBuild='default' />
 // include plug-ins
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
@@ -6,22 +7,13 @@ var concat = require('gulp-concat');
 var order = require('gulp-order');
 var cleanCSS = require('gulp-clean-css');
 var del = require('del');
+var flatten = require('gulp-flatten');
 
-gulp.task('cleanjs', function () {
-   //del is an async function and not a gulp plugin (just standard nodejs)
-   //It returns a promise, so make sure you return that from this task function
-   //  so gulp knows when the delete is complete
-   return del(['app/all.min.js']);
+gulp.task('clean', function () {
+   return del(['app/all.min.js', 'app/all.min.css', 'fonts/*']);
 });
 
-gulp.task('cleancss', function () {
-   //del is an async function and not a gulp plugin (just standard nodejs)
-   //It returns a promise, so make sure you return that from this task function
-   //  so gulp knows when the delete is complete
-   return del(['app/all.min.css']);
-});
-
-gulp.task('min:js', ['cleanjs'], function () {
+gulp.task('min:js', function () {
     console.log('min:js running');
     var bowerJsFiles = mainBowerFiles("**/*.js");
     console.log('bower js files: ', bowerJsFiles);
@@ -31,11 +23,18 @@ gulp.task('min:js', ['cleanjs'], function () {
         .pipe(gulp.dest('app/'));
 });
 
-gulp.task('min:css', ['cleancss'], function () {
+gulp.task('min:css', function () {
     console.log('min:css running');
-    var bowerCssFiles = mainBowerFiles("*.css");
-    console.log('bower css files: ', bowerCssFiles);
-    gulp.src(bowerCssFiles)
+    var cssFiles = ['Content/*'];
+    gulp.src(mainBowerFiles('**/*.css', {
+            overrides: {
+                bootstrap: {
+                    main: [
+                        'dist/css/bootstrap.css',
+                    ]
+                }
+            }
+        }).concat(cssFiles))
         .pipe(concat('all.min.css'))
         .pipe(cleanCSS())
         .pipe(gulp.dest('app/'));
@@ -45,6 +44,13 @@ gulp.task('watch', function () {
     return gulp.watch(mainBowerFiles, ['min:js', 'min:css']);
 });
 
+gulp.task('fonts', function () {
+    gulp.src(['bower_components/**/*.{eot,svg,ttf,woff,woff2}'])
+    .pipe(flatten())
+    .pipe(gulp.dest('fonts/'));
+});
+
+
 //Set a default tasks
-gulp.task('default', ['min:js', 'min:css'], function () { });
+gulp.task('default', ['clean', 'min:js', 'min:css', 'fonts'], function () { });
 
