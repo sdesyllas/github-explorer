9097932444b85082f833daacf198309814b22075
Spyros <spyros.desyllas@outlook.com>
2018-02-27 16:57:57 +0000

    fixed the https call

--- GitHubExplorer.Common/CustomWebClient.cs
+++ GitHubExplorer.Common/CustomWebClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using GitHubExplorer.Abstractions;
 
@@ -10,6 +11,7 @@ namespace GitHubExplorer.Common
         {
             using (var client = new HttpClient())
             {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
